c199cb2f33935d60dab9867799fce92a3fd38ee7
Shikhar Ahuja <ahujashikhar314@gmail.com>
2025-05-29 10:33:28 -0400

    Added memory access, modified build script slightly

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,24 @@
 
 constexpr size_t NUM_EVENTS = 1'000'000;
 constexpr size_t DATA_SIZE = 1024 * 1024; // 1M integers
+std::vector<int> array(DATA_SIZE, 42);
+std::atomic<size_t> global_sum = 0;
 
 int main() {
     // Shared memory block to simulate cache-thrashing access
-    std::vector<int> big_array(DATA_SIZE, 42);
 
     // Atomic counter to track completions
     std::atomic<size_t> completed = 0;
-
     Scheduler scheduler;
     scheduler.start();
     {
         ScopeTimer t("Total Task Scheduling + Execution");
 
         for (size_t i = 1; i <= NUM_EVENTS; ++i) {
-            scheduler.scheduleEvent(Event(i, [&completed]() {
+            scheduler.scheduleEvent(Event(i, [&completed, i]() {
+                size_t local = 0;
+                local += array[(i * 75431) % DATA_SIZE];
+                global_sum.fetch_add(local, std::memory_order_relaxed);
                 completed.fetch_add(1, std::memory_order_relaxed);
             }));
         }
@@ -35,7 +38,7 @@ int main() {
         }
     }
     scheduler.stop();
-
+    //std::cout <<"Global Sum: " << global_sum << std::endl;
 
     return 0;
 }
